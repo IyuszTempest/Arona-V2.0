4b8751eff8cd28641f995e532e1509b4507b10ed
IyuszTempest <aditwibubawang@gmail.com>
2025-09-20 20:24:33 +0700

    fix anime-lolnsfw.js

--- plugins/anime-lolnsfw.js
+++ plugins/anime-lolnsfw.js
@@ -15,46 +15,46 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         },
         message: {
             contactMessage: {
-                vcard: BEGIN:VCARD\nVERSION:3.0\nN:${global.nameowner};Bot;;;\nFN:${global.nameowner}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${global.nameowner};Bot;;;\nFN:${global.nameowner}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
             }
         },
         participant: "0@s.whatsapp.net"
     };
 
     try {
-        await conn.reply(m.chat, _Wait..._, fkontak);
+        await conn.reply(m.chat, `_Wait..._`, fkontak);
 
         let type = command.toLowerCase();
         let apiUrl = '';
 
         if (type === 'loli2') {
-            apiUrl = https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=${API_KEY_LOLHUMAN};
+            apiUrl = `https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=${API_KEY_LOLHUMAN}`;
         } else if (type === 'ahegao') {
-            apiUrl = https://api.lolhuman.xyz/api/random/nsfw/ahegao?apikey=${API_KEY_LOLHUMAN};
+            apiUrl = `https://api.lolhuman.xyz/api/random/nsfw/ahegao?apikey=${API_KEY_LOLHUMAN}`;
         } else if (type === 'ecchi') {
-            apiUrl = https://api.lolhuman.xyz/api/random/nsfw/ecchi?apikey=${API_KEY_LOLHUMAN};
+            apiUrl = `https://api.lolhuman.xyz/api/random/nsfw/ecchi?apikey=${API_KEY_LOLHUMAN}`;
         } else if (type === 'futanari') {
-            apiUrl = https://api.lolhuman.xyz/api/random2/futanari?apikey=${API_KEY_LOLHUMAN};
+            apiUrl = `https://api.lolhuman.xyz/api/random2/futanari?apikey=${API_KEY_LOLHUMAN}`;
         } else if (type === 'eroyuri') {
-            apiUrl = https://api.lolhuman.xyz/api/random2/eroyuri?apikey=${API_KEY_LOLHUMAN};
+            apiUrl = `https://api.lolhuman.xyz/api/random2/eroyuri?apikey=${API_KEY_LOLHUMAN}`;
         } else if (type === 'tits') {
-            apiUrl = https://api.lolhuman.xyz/api/random2/tits?apikey=${API_KEY_LOLHUMAN};
+            apiUrl = `https://api.lolhuman.xyz/api/random2/tits?apikey=${API_KEY_LOLHUMAN}`;
         } else if (type === 'holoero') {
-            apiUrl = https://api.lolhuman.xyz/api/random2/holoero?apikey=${API_KEY_LOLHUMAN};
+            apiUrl = `https://api.lolhuman.xyz/api/random2/holoero?apikey=${API_KEY_LOLHUMAN}`;
         } else if (type === 'erokemo') {
-            apiUrl = https://api.lolhuman.xyz/api/random2/erokemo?apikey=${API_KEY_LOLHUMAN};
+            apiUrl = `https://api.lolhuman.xyz/api/random2/erokemo?apikey=${API_KEY_LOLHUMAN}`;
         } else {
             return conn.reply(m.chat, '❌ Tipe tidak valid!', fkontak);
         }
 
         const res = await fetch(apiUrl);
-        if (!res.ok) throw new Error(API error: ${res.statusText});
+        if (!res.ok) throw new Error(`API error: ${res.statusText}`);
         
         const buffer = await res.buffer();
 
         await conn.sendMessage(m.chat, {
             image: buffer,
-            caption: 😳🤭 Random *${type}* buat bahan sange nih
+            caption: `😳🤭 Random *${type}* buat bahan sange nih`
         }, { quoted: fkontak });
 
     } catch (e) {
