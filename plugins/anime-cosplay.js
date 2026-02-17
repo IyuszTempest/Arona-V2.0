4eb628b48819cc3a2aedb15954358429a29ceaf9
IyuszTempest <aditwibubawang@gmail.com>
2026-02-17 11:50:19 +0700

    Update anime-cosplay.js

--- plugins/anime-cosplay.js
+++ plugins/anime-cosplay.js
@@ -41,7 +41,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 };
 
 handler.help = ['cosplay'];
-handler.tags = ['image', 'premium', 'anime'];
+handler.tags = ['premium', 'anime'];
 handler.command = /^(cosplay)$/i;
 handler.premium = true;
 handler.limit = true;
